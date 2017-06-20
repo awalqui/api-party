09f458c8dcee37e93ee3d9a744b84c90877cb618
awalqui <walqang@gmail.com>
2017-06-20 11:50:51 -0400

    Add route for exact match of /github, i think

--- src/Github.js
+++ src/Github.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import './Github.css'
 
 class Github extends Component {
-    state = {
-        username: ''
-    }
+  state = {
+    username: ''
+  }
 
-    handleChange = (ev) => {
-        const username = ev.currentTarget.value
-        this.setState({ username })
-    }
+  handleChange = (ev) => {
+    const username = ev.currentTarget.value
+    this.setState({ username })
+  }
 
   render() {
     return (
@@ -18,15 +19,16 @@ class Github extends Component {
         <form>
           <div>
             <input 
-                type="text"
-                value={this.state.username}
-                onChange={this.handleChange}
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
             />
           </div>
           <div>
             <button type="submit">Look up Github user</button>
           </div>
         </form>
+        <Route exact path='/github' render={() => <h3>Please enter a username to search on Github</h3>} />
       </div>
     )
   }
